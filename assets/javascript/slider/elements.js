7984ac14a15fcd3e86eac3338128e6bfde22eb31
EspiFreelancer <espifreelancer@gmail.com>
2020-12-15 19:38:58 -0300

    Add JS and CSS logic to animate the slider section

---
+++ assets/javascript/slider/elements.js
@@ -0,0 +1,22 @@
+const elements = [
+	{
+		title: 'Lorem Ipsum',
+		subtitle: 'Ipsum',
+		image: 'images/7.jpg',
+		text: 'Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Facilis, autem totam distinctio, repellendus consequuntur id. Neque sint, tempore eius veniam consequuntur repellendus, inventore quasi voluptatibus aliquid cupiditate eligendi dignissimos dolorem?'
+	},
+	{
+		title: 'Sit Amet',
+		subtitle: 'Adipisicing',
+		image: 'images/5.jpg',
+		text: 'Facilis, autem totam distinctio, repellendus consequuntur id. Neque sint, tempore eius veniam consequuntur repellendus, inventore quasi voluptatibus aliquid cupiditate eligendi dignissimos dolorem?'
+	},
+	{
+		title: 'Voluptatum Laudantium',
+		subtitle: 'Aet consectetur',
+		image: 'images/3.jpg',
+		text: 'Sequi eaque voluptatum laudantium, iusto non. Minima, natus laborum architecto sequi qui sint ipsum ducimus, doloremque, laudantium molestias, deleniti sapiente minus hic.'
+	},
+];
+
+export default elements;
--- assets/javascript/slider/sliderDOM.js
+++ assets/javascript/slider/sliderDOM.js
@@ -1,47 +1,35 @@
-import Slider from './slider.js'
+import Slider from './slider.js';
+import elements from './elements.js';
 
 let sliderTitle = document.querySelector("#slider-title")
 let sliderSubTitle = document.querySelector("#slider-subtitle")
 let sliderText = document.querySelector("#slider-text")
 let sliderImage = document.querySelector("#slider-image")
-
+let textContent = document.querySelector("#slider-text-content");
 
 
 let slider = new Slider({
-	elements: [
-		{
-			title: 'Lorem Ipsum',
-			subtitle: 'Ipsum',
-			image: 'images/3.jpg',
-			text: 'Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Facilis, autem totam distinctio, repellendus consequuntur id. Neque sint, tempore eius veniam consequuntur repellendus, inventore quasi voluptatibus aliquid cupiditate eligendi dignissimos dolorem?'
-		},
-		{
-			title: 'Sit Amet',
-			subtitle: 'Adipisicing',
-			image: 'images/5.jpg',
-			text: 'Facilis, autem totam distinctio, repellendus consequuntur id. Neque sint, tempore eius veniam consequuntur repellendus, inventore quasi voluptatibus aliquid cupiditate eligendi dignissimos dolorem?'
-		},
-		{
-			title: 'Voluptatum Laudantium',
-			subtitle: 'Aet consectetur',
-			image: 'images/7.jpg',
-			text: 'Sequi eaque voluptatum laudantium, iusto non. Minima, natus laborum architecto sequi qui sint ipsum ducimus, doloremque, laudantium molestias, deleniti sapiente minus hic.'
-		},
-	],
+	elements,
 	animationFunc: function(element){
-		sliderTitle.innerHTML = element.title;
-		sliderSubTitle.innerHTML = element.subtitle;
-		sliderText.innerHTML = element.text;
-		sliderImage.src = element.image;
 
-		console.log(element.title);
-		console.log(element.image);
+		textContent.classList.add("hide");
+		sliderImage.classList.add("hide");
+
+		setTimeout(function(){
+			sliderTitle.innerHTML = element.title;
+			sliderSubTitle.innerHTML = element.subtitle;
+			sliderText.innerHTML = element.text;
+			sliderImage.src = element.image;
+
+			textContent.classList.remove("hide");
+			sliderImage.classList.remove("hide");
+		},600);
+
+
 	},
-	speed: 2000
+	speed: 5500
 });
 
 
 
 slider.play();
-
-// setTimeout(slikbder.stop,5000)
